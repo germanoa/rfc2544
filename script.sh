0c53102686e360eed3f2b1b1cf0847eb333ec0ae
Germano Andersson <germanoa@gmail.com>
2014-12-08 17:20:11 -0200

    tempo servico

--- script.sh
+++ script.sh
@@ -26,11 +26,12 @@ do
 	for BYTES in `echo "1518 1280 1024 512 256 128 64"`
 	do
 	
-		#echo "bin/sender $SERVER $PORT $BYTES |tee -a log/$BYTES-$NOW.log"
-		#bin/sender $SERVER $PORT $BYTES |tee -a log/$BYTES-$NOW.log
+		mkdir -p log/$NOW
+		echo "bin/sender $SERVER $PORT $BYTES |tee -a log/$NOW/$BYTES-$NOW.log"
+		bin/sender $SERVER $PORT $BYTES |tee -a log/$NOW/$BYTES-$NOW.log
 	
-		echo "bin/sender $SERVER $PORT $BYTES"
-		bin/sender $SERVER $PORT $BYTES
+		#echo "bin/sender $SERVER $PORT $BYTES"
+		#bin/sender $SERVER $PORT $BYTES
 	
 	done
 
